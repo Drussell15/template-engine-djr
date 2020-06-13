d07469628e2384e13854b45f788c937aa9ad5711
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-06-13 05:02:03 -0700

    prompts
    updated prompt for template

--- app.js
+++ app.js
@@ -3,118 +3,317 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
-const fsPromises = require("fs").promises;
 const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/Renderer");
+const render = require("./lib/htmlRenderer");
+
+let employeeID = 1;
+let employeeList = [];
+
+function managerPrompts() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Manager name: ",
+                name: "managerName"
+            },
+            {
+                type: "input",
+                message: "What is your email address?",
+                name: "managerEmail"
+            },
+            {
+                type: "input",
+                message: "What is your office number?",
+                name: "managerOffice"
+            }
+        ])
+        .then(function (response) {
+            let managerName = response.managerName;
+            let managerEmail = response.managerEmail;
+            let managerOffice = response.managerOffice;
+            let manager = new Manager(
+                managerName,
+                employeeID,
+                managerEmail,
+                managerOffice
+            );
+
+            employeeList.push(manager);
+
+            employeeID++;
+
+            console.log(`
+         
+         Now we'll collect information from you about your employees
+         
+         `);
+
+            employeePrompts();
+        });
+}
+
+function employeePrompts() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What is the employee's role?",
+                choices: ["Engineer", "Intern"],
+                name: "employeeType"
+            },
+            {
+                type: "input",
+                message: "What is the employee's name?",
+                name: "employeeName"
+            },
+            {
+                type: "input",
+                message: "What is the employee's email address?",
+                name: "employeeEmail"
+            }
+        ])
+        .then(function (response) {
+            let employeeType = response.employeeType;
+            let employeeName = response.employeeName;
+            let employeeEmail = response.employeeEmail;
+
+            if (employeeType === "Engineer") {
+                inquirer
+                    .prompt([
+                        {
+                            type: "input",
+                            message: "What is your employee's GitHub username?",
+                            name: "gitHubUN"
+                        },
+                        {
+                            type: "list",
+                            message: "Do you have more employees you'd like to add?",
+                            choices: ["Yes", "No"],
+                            name: "moreEmployees"
+                        }
+                    ])
+                    .then(function (response) {
+                        let employeeGitHub = response.gitHubUN;
+
+                        let engineer = new Engineer(
+                            employeeName,
+                            employeeID,
+                            employeeEmail,
+                            employeeGitHub
+                        );
+
+                        employeeList.push(engineer);
+                        employeeID++;
+
+                        if (response.moreEmployees === "Yes") {
+                            employeePrompts();
+                        } else {
+                            generatePage();
+                            return;
+                        }
+                    });
+            } else {
+                inquirer
+                    .prompt([
+                        {
+                            type: "input",
+                            message: "Where does the intern go to school?",
+                            name: "internSchool"
+                        },
+                        {
+                            type: "list",
+                            message: "Do you have more employees you'd like to add?",
+                            choices: ["Yes", "No"],
+                            name: "moreEmployees"
+                        }
+                    ])
+                    .then(function (response) {
+                        let employeeSchool = response.internSchool;
+
+                        let intern = new Intern(
+                            employeeName,
+                            employeeID,
+                            employeeEmail,
+                            employeeSchool
+                        );
+
+                        employeeList.push(intern);
+
+                        employeeID++;
+
+                        if (response.moreEmployees === "Yes") {
+                            employeePrompts();
+                        } else {
+                            generatePage();
+                            return;
+                        }
+                    });
+            }
+        });
+
+}
+
+function generatePage() {
+    let allCards = "";
+
+    employeeList.forEach(item => {
+        let cardString = item.createCard();
+        allCards += cardString;
+    });
+
+    let fullHTML = `
+   <!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <link
+         rel="stylesheet"
+         href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+         crossorigin="anonymous"
+      />
+      <script
+         src="https://kit.fontawesome.com/ab3fd93a87.js"
+         crossorigin="anonymous"
+      ></script>
+      <title>Team Roster</title>
+   </head>
+   <body>
+      <div
+         class="container-fluid bg-secondary text-center d-flex align-items-center justify-content-center"
+         style="height: 20vh"
+      >
+         <div class="h1 text-white" style="display: inline-block;">
+            My Team
+         </div>
+      </div>
+      <div class="container mt-5">
+         <div class="card-deck d-inline-flex justify-content-center">
+            ${allCards}
+         </div>
+      </div>
+   </body>
+</html>
+   `;
+
+    fs.writeFile("./output/TeamRoster.html", fullHTML, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+}
+
+managerPrompts();
+            //     questions array
+
+            //     const manager = [
+            //         {
+            //             type: "input",
+            //             message: "please enter selected project manager's name.",
+            //             name: "name"
+            //         },
+            //         {
+            //             type: "input",
+            //             message: " Enter selected manager ID number.",
+            //             name: "num"
+            //         },
+            //         {
+            //             type: "input",
+            //             message: "please enter manager email address.",
+            //             name: "email"
+            //         },
+            //         {
+            //             type: "input",
+            //             message: "please enter manager's office number.",
+            //             name: "off"
+            //         },
+            //         {
+            //             type: "list",
+            //             message: "Please enter the next type of member you want to add.",
+            //             name: "position",
+            //             choices: [
+            //                 "Engineer",
+            //                 "Intern",
+            //                 "I am finished adding members to my team"
+            //             ]
+            //         }
+            //     ];
+
+            //     const engineer = [
+            //         {
+            //             type: "input",
+            //             message: "please enter name of engineer",
+            //             name: "name"
+            //         },
+            //         {
+            //             type: "input",
+            //             message: "Please enter your engineer's ID number.",
+            //             name: "num"
+            //         },
+            //         {
+            //             type: "input",
+            //             message: "Please enter your engineer's email.",
+            //             name: "email"
+            //         },
+            //         {
+            //             type: "input",
+            //             message: "Please enter your engineer's GitHub username.",
+            //             name: "git"
+            //         },
+            //         {
+            //             type: "list",
+            //             message: "Please enter the next type of team member you would like to add.",
+            //             name: "position",
+            //             choices: [
+            //                 "Engineer",
+            //                 "Intern",
+            //                 "I am done creating my team."
+            //             ]
+            //         }
+            //     ];
+            //     const intern = [
+            //         {
+            //             type: "input",
+            //             message: "Please enter your intern's name.",
+            //             name: "name"
+            //         },
+            //         {
+            //             type: "input",
+            //             message: "Please enter your intern's ID number.",
+            //             name: "num"
+            //         },
+            //         {
+            //             type: "input",
+            //             message: "Please enter your intern's email.",
+            //             name: "email"
+            //         },
+            //         {
+            //             type: "input",
+            //             message: "Please enter your intern's school name.",
+            //             name: "school"
+            //         },
+            //         {
+            //             type: "list",
+            //             message: "Please enter the next type of team member you would like to add.",
+            //             name: "position",
+            //             choices: [
+            //                 "Engineer",
+            //                 "Intern",
+            //                 "I am done creating my team."
+            //             ]
+            //         }
+            //     ];
 
 
-//questions array
-
-const manager = [
-    {
-        type: "input",
-        message: "please enter selected project manager's name.",
-        name: "name"
-    },
-    {
-        type: "input",
-        message: " Enter selected manager ID number.",
-        name: "num"
-    },
-    {
-        type: "input",
-        message: "please enter manager email address.",
-        name: "email"
-    },
-    {
-        type: "input",
-        message: "please enter manager's office number.",
-        name: "off"
-    },
-    {
-        type: "list",
-        message: "Please enter the next type of member you want to add.",
-        name: "position",
-        choices: [
-            "Engineer",
-            "Intern",
-            "I am finished adding members to my team"
-        ]
-    }
-];
-
-const engineer = [
-    {
-        type: "input",
-        message: "please enter name of engineer",
-        name: "name"
-    },
-    {
-        type: "input",
-        message: "Please enter your engineer's ID number.",
-        name: "num"
-    },
-    {
-        type: "input",
-        message: "Please enter your engineer's email.",
-        name: "email"
-    },
-    {
-        type: "input",
-        message: "Please enter your engineer's GitHub username.",
-        name: "git"
-    },
-    {
-        type: "list",
-        message: "Please enter the next type of team member you would like to add.",
-        name: "position",
-        choices: [
-            "Engineer",
-            "Intern",
-            "I am done creating my team."
-        ]
-    }
-];
-const intern = [
-    {
-        type: "input",
-        message: "Please enter your intern's name.",
-        name: "name"
-    },
-    {
-        type: "input",
-        message: "Please enter your intern's ID number.",
-        name: "num"
-    },
-    {
-        type: "input",
-        message: "Please enter your intern's email.",
-        name: "email"
-    },
-    {
-        type: "input",
-        message: "Please enter your intern's school name.",
-        name: "school"
-    },
-    {
-        type: "list",
-        message: "Please enter the next type of team member you would like to add.",
-        name: "position",
-        choices: [
-            "Engineer",
-            "Intern",
-            "I am done creating my team."
-        ]
-    }
-];
-        
-
-    }
-]
+            // }
+            //         ]
 //this webpage is supposed to generate a webpage that displays a teams basic info so that user can have quick assess to basic information.
 //using inpuirer program will gather info about the dev team and create objects for each memeber (use correct classes)
 
