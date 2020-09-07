f7c9ee0a755e20dd262fe51eb2f3a67940f0eba9
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-09-06 21:27:51 -0700

    added more required

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const jest = require("jest");
+const util = require("util");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const html = require("./templates/templateHTML");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
