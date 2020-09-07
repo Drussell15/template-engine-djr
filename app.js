3ce424a417de802a9fdac8c009efe1e02fc56095
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-09-06 21:43:33 -0700

    fixing  path issues in manager questions

--- app.js
+++ app.js
@@ -1,39 +1,36 @@
+//node packages
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const jest = require("jest");
-const util = require("util");
+
+//required roles
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const html = require("./templates/templateHTML");
 
-const writeFileAsync = util.promisify(fs.writeFile);
-
-let teamArray = [];
-let teamString = ``;
-
-// const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
 //asking questions then create and push the list then start addmember.
+//empty team to push new team members to
+let teamList= [];
 
 function managerPrompts() {
-    inquirer
-        .prompt([
+    inquirer.prompt([
             {
                 type: "input",
-                message: "what is the Manager name: ",
-                name: "managerName"
+                name: "managerName",
+                message: "what is the Manager name: ",    
             },
+
             {
                 type: "input",
+                name: "managerEmail",
                 message: "What is your email address?",
-                name: "managerEmail"
             },
-        {
+            {
                 type: "input",
                 message: "What is your identification number?",
                 name: "managerId"
